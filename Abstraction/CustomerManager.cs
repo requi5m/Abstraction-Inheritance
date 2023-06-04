780965d8c9e632fad43cf05421229fd22e2fcbac
MertAltinay <altinaymert84@hotmail.com>
2023-06-04 16:49:10 +0300

    Proje dosyası ekle.

---
+++ Abstraction/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class CustomerManager
+    {
+        public void Add(List<Customer> customer)
+        {
+            foreach (var customers in customer)
+            {
+                Console.WriteLine(customers.Id);
+            }
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new RealCustomer()
+            {
+                Id = 1, CustomerNo = "123", Name = "Yigit", LastName = "Altinay"
+            };
+            Customer customer2 = new NonRealCustomer()
+            {
+                Id = 2, CustomerNo = "1234", CompanyName = "Company", TaxName = "12345"
+            };
+
+            //I made this here myself if you want you can delete it or add other methods.
+            List<Customer> customers = new List<Customer>() {customer1, customer2 };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customers);
+        }
+    }
+}
